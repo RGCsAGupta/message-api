cbda716c3eff98bc14160d06f2fb2e30df0c5009
Akash Gupta <gupta52akash@hotmail.com>
2021-02-01 23:37:01 -0500

    basic express app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// initialize parsers
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+module.exports = app;
