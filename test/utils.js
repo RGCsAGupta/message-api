479ee4b2d695dcb7728e4c82c32dee3a9d30a127
Akash Gupta <gupta52akash@hotmail.com>
2021-02-03 06:53:35 -0500

    helper functions for tests

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,9 @@
-const supertest = require("supertest");
+const supertest = require('supertest');
 const app = require('../src/app');
 
-module.exports.getAgent = () => supertest.agent(app);
+const agent = supertest.agent(app);
+module.exports.getAuthorizedAgent = () => agent.auth('super-secret-test-token', { type: 'bearer' });
+
+module.exports.getUnAuthorizedAgent = () => agent.auth('invalid', { type: 'bearer' });
+
+module.exports.getNoAuthAgent = () => agent;
